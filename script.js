fa237826bedebf26ee5dc912928a6c7f6d269c3a
ddelvalle3 <ddelvalle3@cps.edu>
2024-12-12 17:05:24 +0000

    x

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 let  left = document.querySelector('.left');
 let  right = document.querySelector('.right');
 let  leftRight = document.querySelector('.leftRight');
-
+let text = document.querySelector('p');+
 let  pizza = document.querySelector('.pizza');
-pizza.style.display ="none";
+    pizza.style.display ="none";
+let burp = document.querySelector('.burp'); 
+    burp.style.display ="none";
 
 left.addEventListener('click', function() {
 
+    console.log("left was clicked")
+    pizza.style.display="block";
     leftRight.style.display ="none";
     left.style.display ="none";
     right.style.display ="none";
-   
-    pizza.style.display ="block";
+        text.innerHTML = "click to eat or search for cheese"; 
 
 });
+
+// pizza left choice 1 pizza left choice 1 pizza left choice 1 pizza left choice one 
+
+
+
+
+pizza.addEventListener('dbclick', function() {
+    console.log("pizza dbl clicked")
+    pizza.style.display = "none";
+    left.style.display = "none";
+    right.style.display = "none";
+
+});
+
+// cheese left choice 2
